5b94a8830dbe7250cfe74e3078092a7ca6f0b899
Luca <luca26712@gmail.com>
2017-05-22 17:20:10 +0200

    Add files via upload

---
+++ Pesaresi.Luca.5i.Calendar/Pesaresi.Luca.5i.Calendar/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Shapes;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using Pesaresi.Luca._5i.Calendar.Models;
+
+namespace Pesaresi.Luca._5i.Calendar
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API WPF Application";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = System.IO.Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            //Creazione API Google Calendar.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            //Definizione parametri di richiesta.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 50;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            //Lista eventi
+            Events events = request.Execute();
+            List<Riga> items = new List<Riga>();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    items.Add(new Riga() { Descrizione = eventItem.Summary, Data = when });
+                }
+                lvEventi.ItemsSource = items;
+            }
+            else
+            {
+                lvEventi.Items.Add("Non ci sono eventi in programma");
+                lvEventi.ItemsSource = lvEventi.Items;
+            }
+        }
+    }
+}
---
+++ Pesaresi.Luca.5i.Calendar/Pesaresi.Luca.5i.Calendar/Models/Riga.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesaresi.Luca._5i.Calendar.Models
+{
+    class Riga
+    {
+        public string Descrizione { get; set; }
+        public string Data { get; set; }
+    }
+}
